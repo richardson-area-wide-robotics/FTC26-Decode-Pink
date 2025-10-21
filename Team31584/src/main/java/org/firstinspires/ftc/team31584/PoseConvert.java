30d12ccdc713609912afe3e74a91745103d45c14
strubium <113206902+strubium@users.noreply.github.com>
2025-10-21 17:50:07 -0500

    Fix spelling mistakes in PoseConvert

--- Team31584/src/main/java/org/firstinspires/ftc/team31584/PoseConvert.java
+++ Team31584/src/main/java/org/firstinspires/ftc/team31584/PoseConvert.java
@@ -9,20 +9,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 public class PoseConvert {
 
-    private static TileQuard pretileQuard;
-    private static HardwareMap hardwareMap;
+    private static TileCord pretileCoord;
     private static IMU imu;
 
 
-    public static TileQuard getPretileQuard() {
-        return pretileQuard;
+    public static TileCord getPretileCoord() {
+        return pretileCoord;
     }
 
     public static void init(IMU imu2){
         imu = imu2;
     }
 
-    public static TileQuard covertToTileQuard(Pose3D pose3D){
+    public static TileCord covertToTileCoord(Pose3D pose3D){
         double theta = pose3D.getOrientation().getYaw();
         double hypot = pose3D.getPosition().y;
 
@@ -34,34 +33,34 @@ public class PoseConvert {
                 141-(12+hypot*Math.sin(theta+imu.getRobotYawPitchRollAngles().getYaw())),0);
         position.toUnit(DistanceUnit.INCH);
 
-        pretileQuard = new TileQuard(position);
+        pretileCoord = new TileCord(position);
 
-        return pretileQuard;
+        return pretileCoord;
     }
 
-    public static TileQuard covertToTileQuard(Position position){
+    public static TileCord covertToTileCoord(Position position){
         position.toUnit(DistanceUnit.INCH);
 
-        pretileQuard = new TileQuard(position);
+        pretileCoord = new TileCord(position);
 
-        return pretileQuard;
+        return pretileCoord;
     }
 
 
-    public static class TileQuard{
+    public static class TileCord {
 
         //The Grid Pose X (Up/Down)
         public int x;
         //The Grid Pose Z (Left/Right)
         public int z;
 
-        public TileQuard (Position position){
+        public TileCord(Position position){
 
 
-            int tileSizeInchines = 23;
+            int tileSizeInches = 23;
 
-            this.x = Math.toIntExact(Math.round(position.x / tileSizeInchines));
-            this.z = Math.toIntExact(Math.round(position.y/tileSizeInchines));
+            this.x = Math.toIntExact(Math.round(position.x / tileSizeInches));
+            this.z = Math.toIntExact(Math.round(position.y/tileSizeInches));
 
 
 
