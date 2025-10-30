84bf8cceb5098e3fa5ee3e5631d1df0a36158004
strubium <113206902+strubium@users.noreply.github.com>
2025-10-30 16:46:37 -0500

    Revert "Current Limit for shooter"
    This reverts commit d89ba034e2ef1af239132b1c11eed3224914557d.

--- Team31584/src/main/java/org/firstinspires/ftc/team31584/Shooter.java
+++ Team31584/src/main/java/org/firstinspires/ftc/team31584/Shooter.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.team31584;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.team31584.abs.EasyPowerTrackingSubsystem;
 
 public class Shooter extends EasyPowerTrackingSubsystem {
 
-    private final DcMotorEx shooterMotor;
-    private final double CURRENT_LIMIT_AMPS = 200.0; // desired current limit
+    private final DcMotor shooterMotor;
 
     /**
      * Constructor for the Shooter subsystem
@@ -16,12 +13,8 @@ public class Shooter extends EasyPowerTrackingSubsystem {
      * @param shooterMotor The motor that controls the shooter wheel
      */
     public Shooter(DcMotor shooterMotor) {
-        this.shooterMotor = (DcMotorEx) shooterMotor;
+        this.shooterMotor = shooterMotor;
         this.shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Set current alert
-        this.shooterMotor.setCurrentAlert(CURRENT_LIMIT_AMPS, CurrentUnit.AMPS);
-        this.shooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
 
     /**
@@ -30,13 +23,15 @@ public class Shooter extends EasyPowerTrackingSubsystem {
      * @param power Power to apply (positive = shoot outward)
      */
     public void shoot(double power) {
-        // Stop motor if overcurrent
-        if (!shooterMotor.isOverCurrent()) {
-            this.setMekSpeed(power);
-            shooterMotor.setPower(-power);
-        }
+        this.setMekSpeed(power);
+        shooterMotor.setPower(-power);
     }
-    public double getCurrent() {
-        return shooterMotor.getCurrent(CurrentUnit.AMPS);
+
+    /**
+     * Stop the shooter motor.
+     */
+    public void stop() {
+        this.setMekSpeed(0);
+        shooterMotor.setPower(0);
     }
 }
